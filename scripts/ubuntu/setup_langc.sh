442bb37da97e615d5831b4f8f1d5c188b5a1e528
soramakura <soramakura418@gmail.com>
2024-07-25 23:06:38 +0900

    インストールするclangのバージョンを18にするようにした

--- scripts/ubuntu/setup_langc.sh
+++ scripts/ubuntu/setup_langc.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -xe
 
-sudo apt install -y \
-    build-essential \
-    make \
-    clang \
-    lldb \
-    lld
+sudo apt install -y build-essential make wget
+
+wget https://apt.llvm.org/llvm.sh
+chmod u+x llvm.sh
+sudo ./llvm.sh 18 all
+rm ./llvm.sh
+
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-18 1
+sudo update-alternatives --install /usr/bin/clang clang++ /usr/bin/clang++-18 1
+sudo update-alternatives --install /usr/bin/clang lldb /usr/bin/lldb-18 1
+sudo update-alternatives --install /usr/bin/clang lld /usr/bin/lld-18 1
