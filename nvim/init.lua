a569be35d851b3a9838935324192e6c575b74411
soramakura <100016927+soramakura@users.noreply.github.com>
2024-07-30 23:23:05 +0900

    add: neovim options

---
+++ nvim/init.lua
@@ -0,0 +1,1 @@
+require("soramakura/core/options")
---
+++ nvim/lua/soramakura/core/options.lua
@@ -0,0 +1,68 @@
+local opt = vim.opt
+
+-- charset
+vim.scriptencoding = 'utf-8'
+opt.encoding = 'utf-8'
+opt.fileencodings = { 'utf-8', 'ucs-bom', 'cp932', 'default' }
+opt.fileencoding = 'utf-8'
+opt.autoread = true
+
+-- prevent full-width characters from being drawn with the width of half-width characters
+opt.ambiwidth = 'double'
+
+-- indent settings
+opt.autoindent = true
+opt.smartindent = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+-- highlight settings
+opt.cursorline = true
+opt.signcolumn = 'yes'
+opt.hlsearch = true
+opt.showmatch = true
+opt.syntax = 'yes'
+
+-- searching settings
+opt.ignorecase = true
+opt.smartcase = true
+opt.incsearch = true
+opt.wrapscan = true
+
+-- clipbard settings
+opt.clipboard = 'unnamedplus'
+
+-- use mouse
+opt.mouse = 'a'
+opt.title = true
+
+-- show line numbers
+opt.number = true
+opt.relativenumber = true
+
+-- show control characters
+opt.list = true
+opt.listchars = {
+    tab = '»-',
+    -- eol = '¬',
+    space = '･',
+    extends = '»',
+    precedes = '«',
+}
+
+opt.laststatus = 2
+opt.cmdheight = 2
+opt.showcmd = true
+
+-- do not generate backup and swap file
+opt.backup = false
+opt.swapfile = false
+opt.hidden = true
+
+-- others
+opt.helplang = { 'ja', 'en' }
+opt.confirm = true
+opt.termguicolors = true
+opt.updatetime = 250
+
