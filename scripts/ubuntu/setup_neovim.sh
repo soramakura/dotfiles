035611bd8ab5652208b8465c06ca742824c3784e
soramakura <soramakura418@gmail.com>
2024-07-30 22:38:05 +0900

    fix: install neovim latest version

--- scripts/ubuntu/setup_neovim.sh
+++ scripts/ubuntu/setup_neovim.sh
@@ -3,7 +3,9 @@
 # Install Neovim latest version
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-sudo ./nvim.appimage
+sudo ./nvim.appimage --appimage-extract
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 rm ./nvim.appimage
 
 # Install deno
