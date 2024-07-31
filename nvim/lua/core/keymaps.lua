98c6a6757ca416176199bd8bb913d9d1723ef2f4
soramakura <soramakura418@gmail.com>
2024-07-31 15:08:01 +0900

    nvim: review key-configration etc

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,56 +1,60 @@
-local set_keymap = vim.api.nvim_set_keymap
+local set_keymap = vim.keymap.set
 
 local function opts(desc)
     return { desc = desc, noremap = true, silent = true }
 end
 
-local default_opts = { silent = true, noremap = true}
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-vim.g.mapleader = ' '
+-- back to normal mode quickly
+set_keymap("i", "jj", "<Esc>", opts("Go to normal mode quickly"))
 
--- 素早くノーマルモードに戻る
-set_keymap('i', 'jj', '<Esc>', { silent = true })
+-- Intuitive Redo
+set_keymap("n", "U", "<C-r>", opts("Redo"))
 
--- 表示行で移動
-set_keymap('n', 'k', 'gk', default_opts)
-set_keymap('n', 'j', 'gj', default_opts)
-set_keymap('n', '0', 'g0', default_opts)
-set_keymap('n', '^', 'g^', default_opts)
-set_keymap('n', '$', 'g$55r', default_opts)
+-- Move by screen line
+set_keymap("n", "k", "gk", opts("Move up"))
+set_keymap("n", "j", "gj", opts("Move down"))
+set_keymap("n", "0", "g0", opts("Move to beginning of line"))
+set_keymap("n", "^", "g^", opts("Move to beginning of line without whitespaces"))
+set_keymap("n", "$", "g$55r", opts("Move to end of line"))
 
--- ウィンドウ分割系
-set_keymap('n', '<leader>sv', '<C-w>v', opts('split window vertically'))
-set_keymap('n', '<leader>sh', '<C-w>s', opts('split window horizontally'))
+-- Window splitting
+set_keymap("n", "<leader>sv", "<C-w>v", opts("Split window vertically"))
+set_keymap("n", "<leader>sh", "<C-w>s", opts("Split window horizontally"))
 
--- ウィンドウ間の移動
-set_keymap('n', '<C-h>', '<C-w>h', opts('move to left window'))
-set_keymap('n', '<C-j>', '<C-w>j', opts('move to bottom window'))
-set_keymap('n', '<C-k>', '<C-w>k', opts('move to top window'))
-set_keymap('n', '<C-l>', '<C-w>l', opts('move to right window'))
+-- Move between windows
+set_keymap("n", "<C-h>", "<C-w>h", opts("Move to left window"))
+set_keymap("n", "<C-j>", "<C-w>j", opts("Move to bottom window"))
+set_keymap("n", "<C-k>", "<C-w>k", opts("Move to top window"))
+set_keymap("n", "<C-l>", "<C-w>l", opts("Move to right window"))
 
--- タブの削除
-set_keymap('n', '<leader><Tab><Tab>', ':tabnew<CR>', opts('create new tab'))
-set_keymap('n', '<leader><Tab>d', ':bd<CR>', opts('delete current tab'))
-set_keymap('n', '<leader><Tab>]', ':bnext<CR>', opts('move to next tab'))
-set_keymap('n', '<leader><Tab>[', ':bprevious<CR>', opts('move to previous tab'))
+-- Tab deletion
+set_keymap("n", "<leader><Tab>n", ":tabnew<CR>", opts("Create new tab"))
+set_keymap("n", "<leader><Tab>d", ":bd<CR>", opts("Delete current tab"))
+set_keymap("n", "<leader><Tab>]", ":bnext<CR>", opts("Move to next tab"))
+set_keymap("n", "<leader><Tab>[", ":bprevious<CR>", opts("Move to previous tab"))
 
--- LSPに関連するキーマップ
-local lsp_opts = { noremap = true, silent = true }
+-- LSP keymaps
+set_keymap("n", "gD", vim.lsp.buf.declaration, opts("Go to declaration"))
+set_keymap("n", "gd", vim.lsp.buf.definition, opts("Go to definition"))
+set_keymap("n", "gt", vim.lsp.buf.type_definition, opts("Go to type definition"))
+set_keymap("n", "gi", vim.lsp.buf.implementation, opts("Go to implementation"))
+set_keymap("n", "gr", vim.lsp.buf.references, opts("Go to references"))
 
-set_keymap('n', 'gD', '<CMD>lua vim.lsp.buf.declaration()<CR>', lsp_opts)
-set_keymap('n', 'gd', '<CMD>lua vim.lsp.buf.definition()<CR>', lsp_opts)
-set_keymap('n', 'gt', '<CMD>lua vim.lsp.buf.type_definition()<CR>', lsp_opts)
-set_keymap('n', 'gi', '<CMD>lua vim.lsp.buf.implementation()<CR>', lsp_opts)
-set_keymap('n', 'gr', '<CMD>lua vim.lsp.buf.references()<CR>', lsp_opts)
+set_keymap("n", "ge", vim.diagnostic.open_float, opts("Show errors on float window"))
+set_keymap("n", "g]", vim.diagnostic.goto_next, opts("Go to next error"))
+set_keymap("n", "g[", vim.diagnostic.goto_prev, opts("Go to prev error"))
 
-set_keymap('n', '<leader>rn', '<CMD>lua vim.lsp.buf.rename()<CR>', lsp_opts)
-set_keymap('n', '<leader>ca', '<CMD>lua vim.lsp.buf.code_action()<CR>', lsp_opts)
-set_keymap('n', '<leader>cf', '<CMD>lua vim.lsp.buf.format()<CR>', lsp_opts)
+set_keymap("n", "gn", vim.lsp.buf.rename, opts("Rename the symbol"))
+set_keymap("n", "ga", vim.lsp.buf.code_action, opts("Run code action"))
+set_keymap("n", "gf", vim.lsp.buf.format, opts("Format code"))
 
-set_keymap('n', 'K', '<CMD>lua vim.lsp.buf.hover()<CR>', lsp_opts)
-set_keymap('n', '<C-k>', '<CMD>lua vim.lsp.buf.signature_help()<CR>', lsp_opts)
+set_keymap("n", "K", vim.lsp.buf.hover, opts("Show popup menu"))
+-- set_keymap("n", "K", vim.lsp.buf.signature_help, opts("Show signature help"))
 
-set_keymap('n', '<leader>wa', '<CMD>lua vim.lsp.buf.add_workspace_folder()<CR>', lsp_opts)
-set_keymap('n', '<leader>wr', '<CMD>lua vim.lsp.buf.remove_workspace_folder()<CR>', lsp_opts)
-set_keymap('n', '<leader>wl', '<CMD>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', lsp_opts)
+set_keymap("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts("Add workspace folder"))
+set_keymap("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts("Remove workspace folder"))
+set_keymap("n", "<leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts("Print the list of workspace folder"))
 
