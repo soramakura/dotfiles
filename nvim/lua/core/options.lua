5a7fa14ad89c828452e090c6429900cf4db06466
soramakura <100016927+soramakura@users.noreply.github.com>
2024-07-31 15:08:01 +0900

    nvim: review key-configration etc

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,28 +1,29 @@
 local opt = vim.opt
 
 -- charset
-vim.scriptencoding = 'utf-8'
-opt.encoding = 'utf-8'
-opt.fileencodings = { 'utf-8', 'ucs-bom', 'cp932', 'default' }
-opt.fileencoding = 'utf-8'
+vim.scriptencoding = "utf-8"
+opt.encoding = "utf-8"
+opt.fileencodings = { "utf-8", "ucs-bom", "cp932", "default" }
+opt.fileencoding = "utf-8"
 opt.autoread = true
 
 -- prevent full-width characters from being drawn with the width of half-width characters
-opt.ambiwidth = 'double'
+opt.ambiwidth = "double"
 
 -- indent settings
 opt.autoindent = true
 opt.smartindent = true
+opt.breakindent = true
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
 
 -- highlight settings
 opt.cursorline = true
-opt.signcolumn = 'yes'
+opt.signcolumn = "yes"
 opt.hlsearch = true
 opt.showmatch = true
-opt.syntax = 'yes'
+opt.syntax = "yes"
 
 -- searching settings
 opt.ignorecase = true
@@ -31,37 +32,43 @@ opt.incsearch = true
 opt.wrapscan = true
 
 -- clipbard settings
-opt.clipboard = 'unnamedplus'
+opt.clipboard = "unnamedplus"
 
 -- use mouse
-opt.mouse = 'a'
+opt.mouse = "a"
 opt.title = true
 
 -- show line numbers
 opt.number = true
+opt.relativenumber = true
 
 -- show control characters
 opt.list = true
 opt.listchars = {
-    tab = '»-',
-    -- eol = '¬',
-    trail = '･',
-    extends = '»',
-    precedes = '«',
+    tab = "»-",
+    -- eol = "¬",
+    space = "･",
+    extends = "»",
+    precedes = "«",
 }
 
 opt.laststatus = 2
-opt.cmdheight = 2
+opt.cmdheight = 1
 opt.showcmd = true
 
 -- do not generate backup and swap file
 opt.backup = false
 opt.swapfile = false
 opt.hidden = true
+opt.undofile = true
+
+-- timeout
+opt.updatetime = 250
+opt.timeout = true
+opt.timeoutlen = 300
 
 -- others
-opt.helplang = { 'ja', 'en' }
+opt.helplang = { "ja", "en" }
 opt.confirm = true
 opt.termguicolors = true
-opt.updatetime = 300
 
