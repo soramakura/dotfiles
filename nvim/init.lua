574754679856c37b7992fd8f8be6a36203c45356
soramakura <soramakura418@gmail.com>
2024-07-31 22:24:16 +0900

    nvim: Move lua/core and lua/plugins into lua/soramakura/

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
-require("core.options")
-require("core.keymaps")
-require("core.lazy")
+require("soramakura.core.options")
+require("soramakura.core.keymaps")
+require("soramakura.core.lazy")
 
--- nvim/lua/soramakura/core/lazy.lua
+++ nvim/lua/soramakura/core/lazy.lua
@@ -11,7 +11,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", {
+require("lazy").setup("soramakura/plugins", {
   defaults = {
     lazy = true,
     version = false,
