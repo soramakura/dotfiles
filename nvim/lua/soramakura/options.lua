722d2a2c9769cdf3070969e29f9b22d370ffa3e8
soramakura <soramakura418@gmail.com>
2024-08-22 16:35:04 +0900

    nvim: toggleterm: Made powershell run on Windows.

--- nvim/lua/soramakura/options.lua
+++ nvim/lua/soramakura/options.lua
@@ -68,7 +68,24 @@ opt.updatetime = 250
 opt.timeout = true
 opt.timeoutlen = 300
 
+-- terminal
+opt.termguicolors = true
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  local pwsh_options = {
+    shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+    shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
+
+  for k, v in pairs(pwsh_options) do
+    opt[k] = v
+  end
+end
+
 -- others
 opt.helplang = { "ja", "en" }
 opt.confirm = true
-opt.termguicolors = true
