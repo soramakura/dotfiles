accc13db2d7fd21c4ef4dc3650cd3ff433890429
soramakura <100016927+soramakura@users.noreply.github.com>
2024-08-27 23:08:33 +0900

    nvim: check rust code with clippy

--- nvim/lua/soramakura/plugins/lspconfig.lua
+++ nvim/lua/soramakura/plugins/lspconfig.lua
@@ -65,6 +65,13 @@ return {
           capabilities = require("cmp_nvim_lsp").default_capabilities(
             vim.lsp.protocol.make_client_capabilities()
           ),
+          settings = {
+            ["rust-analyzer"] = {
+              check = {
+                command = "clippy",
+              },
+            },
+          },
         })
       end,
     })
