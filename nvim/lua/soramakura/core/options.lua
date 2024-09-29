b6340065c986e87fcbb4680de6b6d94b03f45d8a
soramakura <soramakura418@gmail.com>
2024-09-30 04:02:57 +0900

    nvim: hidden unloaded buffer

--- nvim/lua/soramakura/core/options.lua
+++ nvim/lua/soramakura/core/options.lua
@@ -7,6 +7,9 @@ vim.opt.fileencodings = { "utf-8", "ucs-bom", "cp932", "default" }
 -- prevent full-width characters from being drawn with the width of half-width characters
 vim.opt.ambiwidth = "double"
 
+-- hidden unloaded buffer
+vim.opt.hidden = true
+
 -- show line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
