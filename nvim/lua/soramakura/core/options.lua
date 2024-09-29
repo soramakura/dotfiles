5f6272a8f99c96c6583e2d33d08852e725999885
soramakura <soramakura418@gmail.com>
2024-09-30 02:34:37 +0900

    nvim: prevent full-width characters from being drawn with the width of half-width characters

--- nvim/lua/soramakura/core/options.lua
+++ nvim/lua/soramakura/core/options.lua
@@ -3,3 +3,6 @@ vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
 vim.opt.fileencodings = "utf-8"
 vim.opt.fileencodings = { "utf-8", "ucs-bom", "cp932", "default" }
+
+-- prevent full-width characters from being drawn with the width of half-width characters
+vim.opt.ambiwidth = "double"
