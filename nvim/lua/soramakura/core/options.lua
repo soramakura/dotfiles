d05d25b978891d3ad6ef5e636461bbb86ae9e43a
soramakura <soramakura418@gmail.com>
2024-09-30 02:38:51 +0900

    nvim: add tab and indentation settings

--- nvim/lua/soramakura/core/options.lua
+++ nvim/lua/soramakura/core/options.lua
@@ -6,3 +6,12 @@ vim.opt.fileencodings = { "utf-8", "ucs-bom", "cp932", "default" }
 
 -- prevent full-width characters from being drawn with the width of half-width characters
 vim.opt.ambiwidth = "double"
+
+-- tab and indentation
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
