cb7e3de585e084c1444b0c93b425d6090b8129e7
soramakura <soramakura418@gmail.com>
2024-09-30 04:08:51 +0900

    nvim: configure to Automatically read files when they are modified by other programs

--- nvim/lua/soramakura/core/options.lua
+++ nvim/lua/soramakura/core/options.lua
@@ -7,6 +7,9 @@ vim.opt.fileencodings = { "utf-8", "ucs-bom", "cp932", "default" }
 -- prevent full-width characters from being drawn with the width of half-width characters
 vim.opt.ambiwidth = "double"
 
+-- Automatically read files when they are modified by other programs
+vim.opt.autoread = true
+
 -- hidden unloaded buffer
 vim.opt.hidden = true
 
