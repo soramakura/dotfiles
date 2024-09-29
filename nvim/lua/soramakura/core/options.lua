8a0d5111a91599243a29dd71772d5702afe0d6d7
soramakura <100016927+soramakura@users.noreply.github.com>
2024-09-30 02:43:13 +0900

    nvim: show line numbers

--- nvim/lua/soramakura/core/options.lua
+++ nvim/lua/soramakura/core/options.lua
@@ -7,6 +7,10 @@ vim.opt.fileencodings = { "utf-8", "ucs-bom", "cp932", "default" }
 -- prevent full-width characters from being drawn with the width of half-width characters
 vim.opt.ambiwidth = "double"
 
+-- show line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 -- tab and indentation
 vim.opt.autoindent = true
 vim.opt.smartindent = true
