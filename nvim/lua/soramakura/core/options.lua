f99f5b8d7ff060e06a9e9817cf4097875f2bbf11
soramakura <100016927+soramakura@users.noreply.github.com>
2024-09-30 16:13:24 +0900

    nvim: add telescope, which has more functions than I excepted!

--- nvim/lua/soramakura/core/options.lua
+++ nvim/lua/soramakura/core/options.lua
@@ -4,9 +4,6 @@ vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
 vim.opt.fileencodings = { "utf-8", "ucs-bom", "cp932", "default" }
 
--- prevent full-width characters from being drawn with the width of half-width characters
-vim.opt.ambiwidth = "double"
-
 -- Automatically read files when they are modified by other programs
 vim.opt.autoread = true
 
