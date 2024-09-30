6b7ecd00e4ffae9847efb69dde26ae8835a0f912
soramakura <soramakura418@gmail.com>
2024-09-30 18:29:25 +0900

    nvim: add lsp and completion!

---
+++ nvim/lua/soramakura/plugins/lspconfig.lua
@@ -0,0 +1,117 @@
+local set_lsp_keymaps = function()
+  local set_keymap = vim.keymap.set
+  local opts = function(desc)
+    return { desc = desc, noremap = true, silent = true }
+  end
+
+  local builtin = require("telescope.builtin")
+  set_keymap("n", "gd", builtin.lsp_definitions, opts("Go to definition"))
+  set_keymap("n", "gy", builtin.lsp_type_definitions, opts("Go to type definition"))
+  set_keymap("n", "gi", builtin.lsp_implementations, opts("Go to implementation"))
+  set_keymap("n", "gr", builtin.lsp_references, opts("Go to references"))
+  set_keymap("n", "<leader>d", function()
+    builtin.diagnostics({ bufnr = 0 })
+  end, opts("Show diagnostics"))
+end
+
+return {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        { path = "luvit-meta/library", word = { "vim%.uv" } },
+      },
+    },
+  },
+  "Bilal2453/luvit-meta",
+  {
+    "williamboman/mason.nvim",
+    event = { "VeryLazy" },
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    },
+    config = function(_, opts)
+      require("mason").setup(opts)
+
+      require("mason-lspconfig").setup({})
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "clangd",
+          "lua_ls",
+          "rust_analyzer",
+          "stylua",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    cmd = {
+      "LspInfo",
+      "LspInstall",
+      "LspStart",
+      "LspRestart",
+    },
+    dependencies = {
+      "williamboman/mason.nvim",
+      {
+        "j-hui/fidget.nvim",
+        opts = {}
+      },
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      local capabilities = require("cmp_nvim_lsp").default_capabilities(
+        vim.lsp.protocol.make_client_capabilities()
+      )
+      require("mason-lspconfig").setup_handlers({
+        function(server_name)
+          -- default handler
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+        ["lua_ls"] = function()
+          lspconfig.lua_ls.setup({
+            settings = {
+              capabilities = capabilities,
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" },
+                },
+              },
+            },
+          })
+        end,
+        ["rust_analyzer"] = function()
+          require("lspconfig").rust_analyzer.setup({
+            capabilities = capabilities,
+            settings = {
+              ["rust-analyzer"] = {
+                check = {
+                  command = "clippy",
+                },
+              },
+            },
+          })
+        end,
+      })
+    end,
+
+    set_lsp_keymaps()
+  },
+}
---
+++ nvim/lua/soramakura/plugins/nvim-cmp.lua
@@ -0,0 +1,83 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = { "InsertEnter", "CmdlineEnter" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-buffer",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-nvim-lua",
+    {
+      "saecki/crates.nvim",
+      tag = "stable",
+      opts = {},
+    },
+    "onsails/lspkind.nvim",
+  },
+  opts = function()
+    local cmp = require("cmp")
+    local lspkind = require("lspkind")
+    return {
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "path" },
+        { name = "luasnip" },
+        { name = "nvim_lua" },
+        { name = "crates" },
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      mapping = {
+        ["<C-u>"] = cmp.mapping.scroll_docs(-2),
+        ["<C-d>"] = cmp.mapping.scroll_docs(2),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-h>"] = cmp.mapping.abort(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        ["<C-l>"] = cmp.mapping.complete(),
+        ["<C-Space>"] = cmp.mapping.complete(),
+      },
+      formatting = {
+        fields = { "abbr", "kind", "menu" },
+        format = lspkind.cmp_format({
+          max_width = 50,
+          ellipsis_char = "...",
+        }),
+      },
+    }
+  end,
+  config = function(_, opts)
+    local cmp = require("cmp")
+    cmp.setup(opts)
+
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      source = {
+        { name = "buffer"},
+      },
+    })
+
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "path" },
+        { name = "cmdline" },
+      },
+    })
+  end,
+}
