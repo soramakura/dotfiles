818c1761f2ec26b3eba2981e681b50010c8a2e2e
soramakura <soramakura418@gmail.com>
2024-09-30 13:43:24 +0900

    nvim: set colorscheme

---
+++ nvim/lua/soramakura/plugins/colorscheme.lua
@@ -0,0 +1,29 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    flavour = "mocha",
+    term_colors = true,
+    styles = {
+      comments = {},
+      conditionals = {},
+      loops = {},
+      functions = {},
+      keywords = {},
+      strings = {},
+      variables = {},
+      numbers = {},
+      booleans = {},
+      properties = {},
+      types = {},
+      operators = {},
+    },
+    integrations = {},
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin")
+  end,
+}
