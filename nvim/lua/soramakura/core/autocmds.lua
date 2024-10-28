de7b3fcb8e9b8771da7f4f44c1062ee4f1895e74
soramakura <soramakura418@gmail.com>
2024-10-28 19:09:14 +0900

    create my own process to toggle terminal buffer

--- nvim/lua/soramakura/core/autocmds.lua
+++ nvim/lua/soramakura/core/autocmds.lua
@@ -29,6 +29,58 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+-- toggle terminal buffer
+local toggle_terminal_buffer = function()
+  -- Returns whether the buffer is a terminal buffer
+  local is_terminal_buffer = function(buffer)
+    local name = vim.api.nvim_buf_get_name(buffer)
+    return name:find("term://", 1, true) ~= nil
+  end
+
+  -- Finds the terminal buffer and return its handle
+  --
+  -- Returns nil if no terminal buffer is open
+  local find_terminal_buffer = function()
+    for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
+      if is_terminal_buffer(buffer) then
+        return buffer
+      end
+    end
+    return nil
+  end
+
+  -- Opens the terminal buffer
+  --
+  -- Creates a new terminal buffer if not terminal buffer is open
+  local open_terminal_buffer = function()
+    local term_buf = find_terminal_buffer()
+    if term_buf == nil then
+      -- create a new terminal buffer
+      vim.api.nvim_command("terminal")
+    else
+      -- switch to already opened terminal buffer
+      vim.api.nvim_set_current_buf(term_buf)
+    end
+
+    -- change to insert mode
+    vim.api.nvim_command("startinsert")
+  end
+
+  -- Switches to the previously opened buffer
+  local switch_to_previous_buffer = function()
+    vim.api.nvim_set_current_buf(1)
+  end
+
+  local current_buffer = vim.api.nvim_get_current_buf()
+  if is_terminal_buffer(current_buffer) then
+    switch_to_previous_buffer()
+  else
+    open_terminal_buffer()
+  end
+end
+
+vim.api.nvim_create_user_command("ToggleTermBuf", toggle_terminal_buffer, {})
+
 -- clear registers on startup
 vim.g.alpha_lower = "abcdefghijklmnopqrstuvwxyz"
 vim.g.alpha_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
