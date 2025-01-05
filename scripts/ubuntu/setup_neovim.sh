c708037a45bf7bf45e21eff880dc20156fa96335
soramakura <soramakura418@gmail.com>
2025-01-06 01:26:36 +0900

    script: deno and dpp.nvim is unnecessary now

--- scripts/ubuntu/setup_neovim.sh
+++ scripts/ubuntu/setup_neovim.sh
@@ -8,34 +8,9 @@ sudo mv squashfs-root /
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 rm ./nvim.appimage
 
-# Install deno
-sudo apt install unzip
-curl -fsSL https://deno.land/x/install/install.sh | sh
-
 echo 'export DENO_INSTALL="$HOME/.deno"' >> ~/.bashrc
 echo 'export PATH="$DENO_INSTALL/bin:$PATH"' >> ~/.bashrc
 
 source ~/.bashrc
-
-# Install denops and dpp.vim
-DOTFILES_DIR=$(pwd)
-
-mkdir -p ~/.cache/dpp/repos/github.com/
-cd ~/.cache/dpp/repos/github.com
-
-mkdir Shougo
-mkdir vim-denops
-
-cd ./Shougo
-git clone https://github.com/Shougo/dpp.vim
-
-git clone https://github.com/Shougo/dpp-ext-installer
-git clone https://github.com/Shougo/dpp-ext-local
-git clone https://github.com/Shougo/dpp-ext-lazy
-git clone https://github.com/Shougo/dpp-ext-toml
-git clone https://github.com/Shougo/dpp-protocol-git
-
-cd ../vim-denops
-git clone https://github.com/vim-denops/denops.vim
 
 cd $DOTFILES_DIR
