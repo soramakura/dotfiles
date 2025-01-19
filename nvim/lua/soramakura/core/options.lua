711b00e3a2ffb01cc551715aaac5e53b83cc42fe
soramakura <soramakura418@gmail.com>
2025-01-19 20:15:46 +0900

    nvim: add border to diagnostics windows

--- nvim/lua/soramakura/core/options.lua
+++ nvim/lua/soramakura/core/options.lua
@@ -123,5 +123,11 @@ end
 
 -- add frame to the hover window
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = "rounded"
+})
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
   border = "rounded",
+})
+vim.diagnostic.config({
+  float = { border = "rounded" },
 })
