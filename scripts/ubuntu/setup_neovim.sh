2db42c9427e568ef2e6a1823365d1c740645191c
soramakura <soramakura418@gmail.com>
2025-05-26 18:19:26 +0900

    enable to install the latest neovim

--- scripts/ubuntu/setup_neovim.sh
+++ scripts/ubuntu/setup_neovim.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -xe
 
 # Install Neovim latest version
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-sudo ./nvim.appimage --appimage-extract
+
+NEOVIM_VERSION=$(curl -s "https://api.github.com/repos/neovim/neovim/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -LO "https://github.com/neovim/neovim/releases/download/v${NEOVIM_VERSION}/nvim-linux-x86_64.appimage"
+chmod u+x nvim-linux-x86_64.appimage
+sudo ./nvim-linux-x86_64.appimage --appimage-extract
 sudo mv squashfs-root /
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
-rm ./nvim.appimage
+rm ./nvim-linux-x86_64.appimage
 
 echo 'export DENO_INSTALL="$HOME/.deno"' >> ~/.bashrc
 echo 'export PATH="$DENO_INSTALL/bin:$PATH"' >> ~/.bashrc
 
 source ~/.bashrc
 
-cd $DOTFILES_DIR
+cd ${DOTFILES_DIR}
