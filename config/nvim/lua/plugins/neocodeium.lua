cdebcdb2488d53de87f23674f6f7e0f6f0582f84
soramakura <100016927+soramakura@users.noreply.github.com>
2025-05-29 01:14:09 +0900

    nvim: neocodeium: show suggestion manually

--- config/nvim/lua/plugins/neocodeium.lua
+++ config/nvim/lua/plugins/neocodeium.lua
@@ -4,9 +4,9 @@ local set_neocodeium_keymaps = function(neocodeium)
     return { desc = desc, noremap = true, silent = true }
   end
 
-  set_keymap("i", "<M-n>", function() neocodeium.cycle(1) end,
+  set_keymap("i", "<M-n>", function() neocodeium.cycle_or_complete(1) end,
     keymap_opts("Next the Codeium suggestion"))
-  set_keymap("i", "<M-p>", function() neocodeium.cycle(-1) end,
+  set_keymap("i", "<M-p>", function() neocodeium.cycle_or_complete(-1) end,
     keymap_opts("Previous the Codeium suggestion"))
   set_keymap("i", "<M-l>", neocodeium.accept, keymap_opts("Accept the Codeium suggestion"))
   set_keymap("i", "<M-e>", neocodeium.clear, keymap_opts("Clear the current Codeium suggestion"))
@@ -17,7 +17,9 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local neocodeium = require("neocodeium")
-    neocodeium.setup()
+    neocodeium.setup({
+      manual = true,
+    })
     set_neocodeium_keymaps(neocodeium)
   end,
 }
