e9ef0ec6ead0865a022417adb8b20c2339385239
soramakura <100016927+soramakura@users.noreply.github.com>
2025-06-21 01:04:34 +0900

    git: configure Git with commands, instead of using .gitconfig file.
    I would like to verify commits with GPG key, but Git writes the
    encryption key to .gitconfig, which is unsecure. So, I should not
    upload .gitconfig.

--- scripts/ubuntu/setup_git.sh
+++ scripts/ubuntu/setup_git.sh
@@ -3,6 +3,17 @@
 # Git
 sudo apt install git
 
+git config --global user.email          'soramakura418@gmail.com'
+git config --global user.name           'soramakura'
+git config --global core.editor         'nvim'
+git config --global core.quotepath      'false'
+git config --global init.defaultBranch  'main'
+git config --global merge.ff            'false'
+git config --global fetch.prune         'true'
+git config --global pull.ff             'only'
+git config --global push.default        'current'
+git config --global color.ui            'auto'
+
 # GitHub CLI
 sudo apt install gh
 gh auth login
