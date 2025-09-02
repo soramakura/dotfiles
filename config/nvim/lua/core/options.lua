96f33c9e6bcb25db9c11bdac27b1a4e1af39f939
soramakura <100016927+soramakura@users.noreply.github.com>
2025-09-02 21:12:45 +0900

    nvim: set cellwidth of characters.
    Some characters are not displayed properly,
    so set cellwidth of characters manually.

--- config/nvim/lua/core/options.lua
+++ config/nvim/lua/core/options.lua
@@ -141,3 +141,44 @@ end
 vim.diagnostic.config({
   float = { border = "rounded" },
 })
+
+-- prevent two-width characters fro mbeing displayed in one-width.
+vim.opt.ambiwidth = "double"
+-- set the width of characters that are not displayed properly by default.
+local cellwidths = vim.fn.getcellwidths()
+local ambiwidth_excepts = {
+  -- one width
+  { 0xe5fa, 0xe6ac, 1 },
+  { 0xe700, 0xe7c5, 1 },
+  { 0xf000, 0xf2e0, 1 },
+  { 0xe200, 0xe2a9, 1 },
+  { 0xf0001, 0xf1af0, 1 },
+  { 0xe300, 0xe3e3, 1 },
+  { 0xf400, 0xf532, 1 },
+  { 0xe0a0, 0xe0a2, 1 },
+  { 0xe0b0, 0xe0b3, 1 },
+  { 0xe0a3, 0xe0a3, 1 },
+  { 0xe0b4, 0xe0c8, 1 },
+  { 0xe0ca, 0xe0ca, 1 },
+  { 0xe0cc, 0xe0d4, 1 },
+  { 0x2b58, 0x2b58, 1 },
+  { 0xf300, 0xf32f, 1 },
+  { 0xe000, 0xe00a, 1 },
+  { 0xea60, 0xebeb, 1 },
+  { 0x2768, 0x2775, 1 },
+  { 0x2500, 0x259f, 1 },
+
+  -- double width
+  { 0x1f300, 0x1f9ff, 2 },
+  { 0x2600, 0x2767, 2 },
+  { 0x2776, 0x27ff, 2 },
+  { 0x2300, 0x23f1, 2 },
+  { 0x23f3, 0x23ff, 2 },
+  { 0x2900, 0x297f, 2 },
+  { 0x2b00, 0x2b57, 2 },
+  { 0x2b59, 0x2bff, 2 },
+}
+for i = 1, #ambiwidth_excepts do
+  table.insert(cellwidths, ambiwidth_excepts[i])
+end
+vim.fn.setcellwidths(cellwidths)
