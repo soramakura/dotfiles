e83d4da00c3900155c3142059fa799d92441e334
soramakura <100016927+soramakura@users.noreply.github.com>
2025-11-11 22:58:22 +0900

    scripts: windows: add a script that install git and github cli

---
+++ scripts/windows/setup_git.bat
@@ -0,0 +1,23 @@
+@echo on
+setlocal
+
+@rem Git
+winget install -e --id=Git.Git
+
+git config --global user.email          "100016927+soramakura@users.noreply.github.com"
+git config --global user.name           "soramakura"
+git config --global core.editor         "nvim"
+git config --global core.quotepath      "false"
+git config --global core.autocrlf       "input"
+git config --global init.defaultBranch  "main"
+git config --global merge.ff            "false"
+git config --global fetch.prune         "true"
+git config --global pull.ff             "only"
+git config --global push.default        "current"
+git config --global color.ui            "auto"
+
+@rem GitHub CLI
+winget install -e --id=GitHub.cli
+gh auth login
+
+endlocal
