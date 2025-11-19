51717864a206fe81fbe5ae70b55d8a003989874d
soramakura <100016927+soramakura@users.noreply.github.com>
2025-11-19 17:48:05 +0900

    scripts: make the script install the cargo apps

--- scripts/ubuntu/setup_rust.sh
+++ scripts/ubuntu/setup_rust.sh
@@ -3,3 +3,17 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup component add rust-analyzer
 rustup component add rustfmt
+
+# For updating apps installed by cargo
+cargo install cargo-update
+
+# For cleaning cargo cache
+cargo install cargo-cache
+cargo install cargo-sweep
+
+# For checking licenses
+cargo install cargo-license
+cargo install cargo-bundle-licenses
+
+# For checking dependencies
+cargo install cargo-tree
