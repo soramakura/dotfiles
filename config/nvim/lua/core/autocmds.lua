abfaae1d9805be03c66d53718efcb25870d85502
soramakura <100016927+soramakura@users.noreply.github.com>
2025-12-07 01:08:02 +0900

    nvim: remove the keymap override for gf because I use gx instead

--- config/nvim/lua/core/autocmds.lua
+++ config/nvim/lua/core/autocmds.lua
@@ -6,15 +6,3 @@ require("core.autocmds.clear-registers").setup()
 
 -- setup toggletermbuf
 require("core.autocmds.toggletermbuf").setup()
-
-vim.api.nvim_create_autocmd("BufReadCmd", {
-  pattern = { "https://*", "http://*" },
-  group = vim.api.nvim_create_augroup(
-    "soramakura::config::core::openbrowser",
-    { clear = true }
-  ),
-  callback = function(args)
-    vim.ui.open(args.match)
-    vim.cmd([[e # | bdelete #]])
-  end
-})
