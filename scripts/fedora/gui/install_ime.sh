bf411e939788ba8cedb706c9e3125c266e4243c6
soramakura <100016927+soramakura@users.noreply.github.com>
2026-02-10 14:41:37 +0900

    fix: scripts(fedora): prevent setting unnecessary envvar for Wayland

--- scripts/fedora/gui/install_ime.sh
+++ scripts/fedora/gui/install_ime.sh
@@ -5,5 +5,3 @@ sudo dnf install fcitx5 fcitx5-configtool fcitx5-autostart fcitx5-mozc
 
 # Set as defaut input method
 echo "export XMODIFIERS=@im=fcitx5" >> ${HOME}/.bash_profile
-echo "export GTK_IM_MODULE=fcitx5" >> ${HOME}/.bash_profile
-echo "export QT_IM_MODULE=fcitx5" >> ${HOME}/.bash_profile
