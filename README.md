9a7ed29fff8dbb31e05e8653bfa93617f76c6686
soramakura <100016927+soramakura@users.noreply.github.com>
2026-02-19 16:50:08 +0900

    nvim: scripts(fedora): stop using Source Han * fonts
    This change is because both Source Han * and Noto * CJK JP are the same
    fonts. Use Noto * CJK JP instead.

--- README.md
+++ README.md
@@ -7,10 +7,9 @@ curl https://raw.githubusercontent.com/soramakura/dotfiles/main/setup_ubuntu.sh 
 ```
 
 ## Fonts
-- https://github.com/yuru7/HackGen/releases
-  - Install `HackGen_NF_v*.zip`
-- https://github.com/adobe-fonts/source-han-sans/releases
-  - Install `Language Specific OTFs Japanese`
-- https://github.com/adobe-fonts/source-han-serif/releases
-  - Install `Language Specifiex OTFs Japanese`
 
+- Noto Sans CJK JP
+- Noto Selif CJK JP
+- HackGen Console NF
+  - https://github.com/yuru7/HackGen/releases
+    - Install `HackGen_NF_v*.zip`
--- scripts/fedora/gui/install_fonts.sh
+++ scripts/fedora/gui/install_fonts.sh
@@ -6,16 +6,4 @@ unzip "./HackGen_NF_${HACKGEN_VERSION}.zip" -d "./temporary"
 find "./temporary" \( -name "*.otf" -o -name "*.ttf" \) -exec sudo mv {} "/usr/local/share/fonts/" \;
 rm -rf "./HackGen_NF_${HACKGEN_VERSION}.zip" "./temporary"
 
-SOURCE_HAN_SERIF_VERSION=$(curl -s "https://api.github.com/repos/adobe-fonts/source-han-serif/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
-curl -LO "https://github.com/adobe-fonts/source-han-serif/releases/download/${SOURCE_HAN_SERIF_VERSION}/07_SourceHanSerifJ.zip"
-unzip "./07_SourceHanSerifJ.zip" -d "./temporary"
-find "./temporary" \( -name "*.otf" -o -name "*.ttf" \) -exec sudo mv {} "/usr/local/share/fonts/" \;
-rm -rf "./07_SourceHanSerifJ.zip" "./temporary"
-
-SOURCE_HAN_SANS_VERSION=$(curl -s "https://api.github.com/repos/adobe-fonts/source-han-sans/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
-curl -LO "https://github.com/adobe-fonts/source-han-sans/releases/download/${SOURCE_HAN_SANS_VERSION}/07_SourceHanSansJ.zip"
-unzip "./07_SourceHanSansJ.zip" -d "./temporary"
-find "./temporary" \( -name "*.otf" -o -name "*.ttf" \) -exec sudo mv {} "/usr/local/share/fonts/" \;
-rm -rf "./07_SourceHanSansJ.zip" "./temporary"
-
 fc-cache -fv
