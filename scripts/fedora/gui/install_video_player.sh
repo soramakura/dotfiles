c755072ac77ffff9570e99c7565b096022bb8349
soramakura <100016927+soramakura@users.noreply.github.com>
2026-02-23 16:22:12 +0900

    scripts(fedora): enable to play H.264 codec
    * RPM Fusion is required to install the full featured ffmpeg
    * `install_video_player.sh` swaps ffmpeg-free and ffmpeg
    * By running `install_video_player.sh`, the environment includes
      non-free software

--- scripts/fedora/gui/install_video_player.sh
+++ scripts/fedora/gui/install_video_player.sh
@@ -3,5 +3,9 @@
 # VLC
 sudo dnf install vlc
 
+# enable RPM Fusion
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+
 # ffmpeg
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing
+sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
