67135a4701817abe70590769c5ff2c83a73d726c
soramakura <100016927+soramakura@users.noreply.github.com>
2026-03-01 16:40:46 +0900

    nvim(options): move the config related to PowerShell down
    I like Microslop Replacer btw.

--- config/nvim/lua/core/options.lua
+++ config/nvim/lua/core/options.lua
@@ -103,24 +103,6 @@ vim.opt.timeoutlen = 300
 -- enable English help
 vim.opt.helplang = { "en" }
 
--- When running on Windows, change the shell from cmd.exe to powershell
-vim.opt.termguicolors = true
-if vim.loop.os_uname().sysname == "Windows_NT" then
-  local pwsh_options = {
-    shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
-    shellcmdflag =
-    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-    shellquote = "",
-    shellxquote = "",
-  }
-
-  for k, v in pairs(pwsh_options) do
-    vim.opt[k] = v
-  end
-end
-
 -- border settings
 vim.o.winborder = "rounded"
 
@@ -172,4 +154,22 @@ local lsp_log = vim.fn.stdpath("state") .. "/lsp.log"
 local stat = vim.uv.fs_stat(lsp_log)
 if stat and (os.time() - stat.mtime.sec) > 86400 then
   os.remove(lsp_log)
+end
+
+-- When running on Windows, change the shell from cmd.exe to powershell
+vim.opt.termguicolors = true
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  local pwsh_options = {
+    shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+    shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
+
+  for k, v in pairs(pwsh_options) do
+    vim.opt[k] = v
+  end
 end
