f7bc76b15b3ff4c45dc51375dfffc1f2b0981dfc
soramakura <100016927+soramakura@users.noreply.github.com>
2026-03-16 13:00:49 +0900

    nvim(snacks): fix an incorrect value is passed as the notification level

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -63,7 +63,7 @@ local lsp_progress_hook = function()
       end, p)
 
       local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-      vim.notify(table.concat(msg, "\n"), "info", {
+      vim.notify(table.concat(msg, "\n"), vim.log.levels.INFO, {
         id = "lsp_progress",
         title = client.name,
         opts = function(notif)
