b1b798deabf15ef5ef2248b5a99ed601c40c83b6
soramakura <100016927+soramakura@users.noreply.github.com>
2026-03-31 23:24:17 +0900

    scripts(fedora): avoid showing the fcitx notification
    Fcitx5 shows a warning notificaion on startup because of environment
    variables. Fcitx5 automatically set them, so the settings variable code
    is removed.

--- scripts/fedora/install_ime.sh
+++ scripts/fedora/install_ime.sh
@@ -2,6 +2,3 @@
 
 # Install Fctix5
 sudo dnf install fcitx5 fcitx5-configtool fcitx5-autostart fcitx5-mozc
-
-# Set as defaut input method
-echo "export XMODIFIERS=@im=fcitx5" >> ${HOME}/.bash_profile
