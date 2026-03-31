ce243c66e158a0fca5f28265807a7f9604840841
soramakura <100016927+soramakura@users.noreply.github.com>
2026-03-31 23:26:40 +0900

    scripts(fedora): install niri without unnecessary dependencies
    niri installs unnecessary dependencies such as waybar. The script
    automatically remove those dependencies.

--- scripts/fedora/install_niri.sh
+++ scripts/fedora/install_niri.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -xeu
 
 sudo dnf install niri
+# Remove unnecessary UIs
+sudo dnf remove alacritty fuzzel swaylock waybar
 
 # Link the config files
 DOTFILES_DIR=$(readlink -f "$(dirname ${BASH_SOURCE[0]})/../../")
