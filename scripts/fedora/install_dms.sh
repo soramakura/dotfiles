c007ff45434d4e11a08e47eaaccc928f6dc0012c
soramakura <100016927+soramakura@users.noreply.github.com>
2026-03-31 23:20:52 +0900

    scripts(fedora): install dms dependencies to work properly

--- scripts/fedora/install_dms.sh
+++ scripts/fedora/install_dms.sh
@@ -4,9 +4,14 @@ DOTFILES_DIR=$(readlink -f "$(dirname ${BASH_SOURCE[0]})/../../")
 
 # Install dependencies
 source "${DOTFILES_DIR}/scripts/fedora/install_niri.sh"
+source "${DOTFILES_DIR}/scripts/fedora/install_power_profiles.sh"
+source "${DOTFILES_DIR}/scripts/fedora/install_foot.sh"
+source "${DOTFILES_DIR}/scripts/fedora/install_fonts.sh"
 
 sudo dnf copr enable avengemedia/dms
-sudo dnf install dms
+sudo dnf install dms qt6ct-kde qt5ct nautilus
+
+systemctl --user enable pipewire pipewire-pulse wireplumber
 systemctl --user add-wants niri.service dms
 
 # Link the config files
