87c9985e22d35152d39ec54095c0d3bb79d3ca95
soramakura <100016927+soramakura@users.noreply.github.com>
2026-03-31 23:21:56 +0900

    scripts(fedora): allow to install on Fedora Everything
    Noto (Sans|Serif) CJK JP and fontconfig are not contained in Fedora
    Everything by default. The script can run on such a environment.

--- scripts/fedora/install_fonts.sh
+++ scripts/fedora/install_fonts.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -xe
 
+sudo mkdir -p "/usr/local/share/fonts"
+
+sudo dnf install google-noto-sans-cjk-vf-fonts google-noto-serif-cjk-vf-fonts
+
 HACKGEN_VERSION=$(curl -s "https://api.github.com/repos/yuru7/HackGen/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
 curl -LO "https://github.com/yuru7/HackGen/releases/download/${HACKGEN_VERSION}/HackGen_NF_${HACKGEN_VERSION}.zip"
 unzip "./HackGen_NF_${HACKGEN_VERSION}.zip" -d "./temporary"
 find "./temporary" \( -name "*.otf" -o -name "*.ttf" \) -exec sudo mv {} "/usr/local/share/fonts/" \;
 rm -rf "./HackGen_NF_${HACKGEN_VERSION}.zip" "./temporary"
+
+sudo dnf install fontconfig
 
 fc-cache -fv
