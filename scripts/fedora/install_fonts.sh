c88ccc7e361f3d9a722e7e878356cf37335f5b65
soramakura <100016927+soramakura@users.noreply.github.com>
2026-04-07 14:13:11 +0900

    scripts(fedora): install fonts for symbols

--- scripts/fedora/install_fonts.sh
+++ scripts/fedora/install_fonts.sh
@@ -2,7 +2,12 @@
 
 sudo mkdir -p "/usr/local/share/fonts"
 
-sudo dnf install google-noto-sans-cjk-vf-fonts google-noto-serif-cjk-vf-fonts twitter-twemoji-fonts
+sudo dnf install google-noto-sans-cjk-vf-fonts \
+    google-noto-serif-cjk-vf-fonts \
+    google-noto-sans-symbols-fonts \
+    google-noto-sans-symbols2-fonts \
+    google-noto-sans-canadian-aboriginal-fonts \
+    twitter-twemoji-fonts
 
 HACKGEN_VERSION=$(curl -s "https://api.github.com/repos/yuru7/HackGen/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
 curl -LO "https://github.com/yuru7/HackGen/releases/download/${HACKGEN_VERSION}/HackGen_NF_${HACKGEN_VERSION}.zip"
