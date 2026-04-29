572728151fcc6708f4cdb131b0d5492f7e720a3b
soramakura <100016927+soramakura@users.noreply.github.com>
2026-04-29 23:51:16 +0900

    script(fedora): remove unnecessary media installation scripts
    The files below are deleted because they are no longer unnecessary.
    
    * `scripts/fedora/install_kde_media_tools.sh`
    * `scripts/fedora/install_vlc.sh`

--- scripts/fedora/install_vlc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xe
-
-DOTFILES_DIR=$(readlink -f "$(dirname ${BASH_SOURCE[0]})/../../")
-
-# Install video codec
-source ${DOTFILES_DIR}/scripts/fedora/install_video_codec.sh
-
-# VLC
-sudo dnf install vlc
