309649430077d07990112a3eefb197f2c4cf139b
soramakura <100016927+soramakura@users.noreply.github.com>
2026-05-16 23:26:17 +0900

    nvim: use tree-sitter-manager.nvim instead of nvim-treesitter
    nvim-treesitter got archived on April 4 2026. tree-sitter-manager is its
    minimal alternative and requires Neovim 0.12 and later.
    
    Today, the Fedora official repo offers Neovim 0.12.2, and the plugin
    started being used.

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,6 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "main",
+  "romus204/tree-sitter-manager.nvim",
   event = { "FileType" },
-  build = ":TSUpdate",
   opts = {
     ensure_installed = {
       "c",
@@ -14,55 +12,16 @@ return {
       "markdown",
       "markdown_inline",
       "git_config",
-      "gitignore",
-      "gitattributes",
-      "diff",
       "git_rebase",
+      "gitattributes",
       "gitcommit",
+      "gitignore",
+      "diff",
     },
+    auto_install = true,
+    highlight = true,
   },
   config = function(_, opts)
-    local treesitter = require("nvim-treesitter")
-    treesitter.setup()
-
-    -- Install the parsers listed in ensure_installed.
-    treesitter.install(opts.ensure_installed or {})
-
-    local ts_available_filetypes = vim.iter(treesitter.get_available())
-        :map(function(lang)
-          return vim.treesitter.language.get_filetypes(lang)
-        end)
-        :flatten()
-        :totable()
-
-    local ts_installed_filetypes = vim.iter(treesitter.get_installed())
-        :map(function(lang)
-          return vim.treesitter.language.get_filetypes(lang)
-        end)
-        :flatten()
-        :totable()
-
-    vim.api.nvim_create_autocmd({ "FileType" }, {
-      group = vim.api.nvim_create_augroup("soramakura::config::plugins::treesitter", {}),
-      pattern = ts_available_filetypes,
-      callback = function(args)
-        local filetype = args.match
-
-        if vim.tbl_contains(ts_installed_filetypes, filetype) then
-          vim.treesitter.start()
-          vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        else
-          treesitter.install(filetype)
-          -- Update filetypes supported by installed parsers.
-          ts_installed_filetypes = vim.iter(treesitter.get_installed())
-              :map(function(lang)
-                return vim.treesitter.language.get_filetypes(lang)
-              end)
-              :flatten()
-              :totable()
-        end
-      end
-    })
+    require("tree-sitter-manager").setup(opts)
   end,
 }
